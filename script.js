672829b382a876950fc3183ee3ad0d2e7a3d6748
Alex Beasley <alexmbeasley@gmail.com>
2023-10-10 13:39:15 -0500

    init

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  //no input
+  if (!guess) {
+    displayMessage("No number ya goof!");
+    //player gets the correct answer
+  } else if (guess === secretNumber) {
+    displayMessage("Great Job!");
+
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "green";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    //guess is too high/low
+  } else if (guess !== secretNumber) {
+    //lets check your score first
+
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("Ya blew it!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  //set everything to beginning values
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".number").textContent = "?";
+  displayMessage("Feed me a guess...");
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector(".number").style.width = "15rem";
+});
